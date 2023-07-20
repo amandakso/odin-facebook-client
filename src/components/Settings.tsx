7ef717edfab600a6dabdee7ecdd138e3c5149886
Amanda So <soamanda95@gmail.com>
2023-07-19 17:30:07 -0700

    add fetch request to update profile bio

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,8 +2,13 @@ import Container from "@mui/material/Container";
 import Divider from "@mui/material/Divider";
 import Alert from "@mui/material/Alert";
 import Snackbar from "@mui/material/Snackbar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 import { useEffect, useState } from "react";
 import jwtDecode, { JwtPayload } from "jwt-decode";
+import { Mouse } from "@mui/icons-material";
 
 declare module "jwt-decode" {
   export interface JwtPayload {
@@ -26,6 +31,72 @@ const Settings = () => {
 
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
+  };
+  // TODO add fetch to update bio in server
+  const updateBio = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const token: string = sessionStorage.getItem("token") as string;
+    const decoded = jwtDecode<JwtPayload>(token);
+    const data = new FormData(event.currentTarget);
+    console.log(data.get("bio"));
+    try {
+      const res = await fetch(
+        `https://odin-facebook-api.onrender.com/api/users/${decoded.user._id}/profile/update-bio`,
+        {
+          method: "PUT",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${token}`,
+          },
+          body: JSON.stringify({
+            bio: data.get("bio"),
+          }),
+        }
+      );
+      const resJson = await res.json();
+
+      if (res.status === 200) {
+        if (resJson.error) {
+          setAlertMessage(resJson.error);
+          if (alertSeverity != "error") {
+            setAlertSeverity("error");
+          }
+          setAlertMessage(resJson.error);
+          setSnackbarOpen(true);
+        } else if (resJson.data) {
+          setAlertMessage(resJson.message);
+          if (alertSeverity != "success") {
+            setAlertSeverity("success");
+          }
+          setSnackbarOpen(true);
+          setProfileBio(resJson.data);
+        } else {
+          return;
+        }
+      } else {
+        if (resJson.errors) {
+          setAlertMessage(
+            "Invalid entry. Profile bio cannot be > 200 characters"
+          );
+        } else {
+          setAlertMessage("An error occurred. Unable to update bio.");
+        }
+        if (alertSeverity != "error") {
+          setAlertSeverity("error");
+        }
+        setSnackbarOpen(true);
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        setAlertMessage(err.message);
+        if (alertSeverity != "error") {
+          setAlertSeverity("error");
+        }
+        setSnackbarOpen(true);
+      }
+    }
   };
 
   // Get current profile info: photo, bio, username
@@ -74,13 +145,57 @@ const Settings = () => {
       }
     };
     fetchProfile();
-  }, []);
+  }, [alertSeverity]);
   return (
     <>
       <Container maxWidth="lg">
         <h1>Account Settings</h1>
         <Divider role="presentation">Update Profile Photo</Divider>
         <Divider role="presentation">Update Profile Bio</Divider>
+        <section>
+          <Grid container columns={2}>
+            <Grid item xs>
+              <Grid container columns={1}>
+                <Grid item xs justifyContent="flex-start">
+                  <p>Current Bio:</p>
+                </Grid>
+                <Grid item xs justifyContent="flex-start">
+                  <div>
+                    <p>{profileBio}</p>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs>
+              <Grid container direction="column" alignItems="flex-start">
+                <Grid item>
+                  <p>New Bio:</p>
+                </Grid>
+                <Grid item>
+                  <Box component="form" onSubmit={updateBio} noValidate>
+                    <Grid container columns={2} alignItems="center" spacing={4}>
+                      <Grid item>
+                        <TextField
+                          margin="normal"
+                          required
+                          id="bio"
+                          label="New Bio: "
+                          name="bio"
+                          autoFocus
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button type="submit" variant="contained">
+                          Update Bio
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </section>
         <Divider role="presentation">Update Username</Divider>
         <Divider role="presentation">Update Password</Divider>
 
