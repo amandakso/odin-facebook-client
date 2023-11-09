a9772f88597d751376faa7ef44dcd2f3387e9e7b
Amanda So <soamanda95@gmail.com>
2023-11-08 18:45:48 -0800

    fetch likes of a post and display number of likes. handle like button click

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,41 +3,121 @@ import Textarea from "@mui/material/TextareaAutosize";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
+import jwtDecode, { JwtPayload } from "jwt-decode";
 
 import ProfilePhoto from "./ProfilePhoto";
 
+declare module "jwt-decode" {
+  export interface JwtPayload {
+    user: {
+      username: string;
+      _id: string;
+    };
+  }
+}
+
 const Post = (props) => {
+  const token: string = sessionStorage.getItem("token") as string;
+  const decoded = jwtDecode<JwtPayload>(token);
   const [photo, setPhoto] = useState<string>("");
   const [readOnly, setReadOnly] = useState<boolean>(true);
+  const [numLikes, setNumLikes] = useState<number>(0);
+  //const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  const fetchProfile = async (authorid: string) => {
+    try {
+      const res = await fetch(
+        `https://odin-facebook-api.onrender.com/api/users/${authorid}/profile`,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const resJson = await res.json();
+      if (resJson.error) {
+        console.log(resJson.error);
+      } else {
+        setPhoto(resJson.photo);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const getPostLikes = async (postid: string) => {
+    try {
+      const res = await fetch(
+        `https://odin-facebook-api.onrender.com/api/posts/${postid}/likes`,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const resJson = await res.json();
+
+      if (resJson.error) {
+        console.log(resJson.error);
+      }
+      const likes = resJson.likes ? resJson.likes.users.length : 0;
+
+      setNumLikes(likes ? likes : 0);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLikeClick = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    const postid = props.postid;
+    if (!postid) {
+      console.log("No postid found");
+      return;
+    }
+    try {
+      const res = await fetch(
+        `https://odin-facebook-api.onrender.com/api/posts/${postid}/likes/like`,
+        {
+          method: "PUT",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${token}`,
+          },
+        }
+      );
+
+      const resJson = await res.json();
+      console.log(resJson);
+      console.log("click");
+
+      if (resJson.error) {
+        console.log(resJson.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    const fetchProfile = async (id: string) => {
-      try {
-        const res = await fetch(
-          `https://odin-facebook-api.onrender.com/api/users/${id}/profile`,
-          {
-            method: "GET",
-            mode: "cors",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const resJson = await res.json();
-        if (resJson.error) {
-          console.log(resJson.error);
-        } else {
-          setPhoto(resJson.photo);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
     if (props.authorid) {
       fetchProfile(props.authorid);
     }
   }, [props.authorid]);
+
+  useEffect(() => {
+    if (props.postid) {
+      getPostLikes(props.postid);
+    }
+  }, [props.postid]);
+
   return (
     <>
       <Box style={{ width: "80%" }}>
@@ -71,7 +151,14 @@ const Post = (props) => {
               </Box>
             </Grid>
             <Grid item style={{ textAlign: "left" }}>
-              <Button type="button" variant="contained">
+              <p>
+                {numLikes} {numLikes === 1 ? "like" : "likes"}
+              </p>
+              <Button
+                type="button"
+                variant="contained"
+                onClick={handleLikeClick}
+              >
                 Like
               </Button>
               <Button type="button" variant="contained">
