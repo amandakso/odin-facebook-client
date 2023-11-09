71b44e18fec4dee86e3f9a2ff9d41525f917d97d
Amanda So <soamanda95@gmail.com>
2023-11-09 15:33:28 -0800

    check if user liked post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -16,13 +16,19 @@ declare module "jwt-decode" {
   }
 }
 
+type likes = {
+  _id: string;
+  username: string;
+};
+
 const Post = (props) => {
   const token: string = sessionStorage.getItem("token") as string;
   const decoded = jwtDecode<JwtPayload>(token);
+  const currentUser = decoded.user._id;
   const [photo, setPhoto] = useState<string>("");
   const [readOnly, setReadOnly] = useState<boolean>(true);
   const [numLikes, setNumLikes] = useState<number>(0);
-  //const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [isLiked, setIsLiked] = useState<boolean>(false);
 
   const fetchProfile = async (authorid: string) => {
     try {
@@ -46,6 +52,7 @@ const Post = (props) => {
       console.log(err);
     }
   };
+
   const getPostLikes = async (postid: string) => {
     try {
       const res = await fetch(
@@ -64,12 +71,21 @@ const Post = (props) => {
       if (resJson.error) {
         console.log(resJson.error);
       }
-      const likes = resJson.likes ? resJson.likes.users.length : 0;
-
-      setNumLikes(likes ? likes : 0);
+      const likes = resJson.likes ? resJson.likes.users : [];
+      checkIfLiked(likes);
+      setNumLikes(likes.length ? likes.length : 0);
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const checkIfLiked = (likes: likes[]) => {
+    likes.forEach((like) => {
+      if (like._id === currentUser) {
+        setIsLiked(true);
+        return;
+      }
+    });
   };
 
   const handleLikeClick = async (
@@ -95,12 +111,14 @@ const Post = (props) => {
       );
 
       const resJson = await res.json();
+      console.log(resJson.likes?.users);
 
       if (resJson.error) {
         console.log(resJson.error);
       }
-      const likes = resJson.likes ? resJson.likes.users.length : 0;
-      setNumLikes(likes ? likes : 0);
+      const likes = resJson.likes ? resJson.likes.users : [];
+      setIsLiked(true);
+      setNumLikes(likes.length ? likes.length : 0);
     } catch (err) {
       console.log(err);
     }
