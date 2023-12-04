571c7aac629265b92937fc4d8df3719db34bc23f
Amanda So <soamanda95@gmail.com>
2023-12-04 15:09:00 -0800

    add details to comment component

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,7 +1,89 @@
+import Box from "@mui/material/Box";
+import Textarea from "@mui/material/TextareaAutosize";
+import Grid from "@mui/material/Grid";
+import { useEffect, useState } from "react";
+
+import ProfilePhoto from "./ProfilePhoto";
+
 const Comment = (props) => {
+  const [photo, setPhoto] = useState<string>("");
+  /**
+   * 
+   *             <Comment
+              key={comment._id}
+              authorid={comment.author._id}
+              username={comment.author.username}
+              createdAt={comment.createdAt}
+              updatedAt={comment.updatedAt}
+              text={comment.text}
+              commentid={comment._id}
+            />
+   */
+
+  const fetchProfile = async (authorid: string) => {
+    try {
+      const res = await fetch(
+        `https://odin-facebook-api.onrender.com/api/users/${authorid}/profile`,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const resJson = await res.json();
+      if (resJson.error) {
+        console.log(resJson.error);
+      } else {
+        setPhoto(resJson.photo);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (props.authorid) {
+      fetchProfile(props.authorid);
+    }
+  }, [props.authorid]);
+
   return (
     <>
-      <h1>Comment</h1>
+      <Box style={{ width: "80%" }}>
+        <Grid container spacing={2}>
+          <Grid container item xs={12} spacing={1}>
+            <Grid item xs="auto">
+              <ProfilePhoto username={props.username} photo={photo} size={75} />
+            </Grid>
+            <Grid item xs={8}>
+              <p style={{ textAlign: "left" }}>{props.username}</p>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid item>
+              <Box component="form" noValidate>
+                <Textarea
+                  aria-label="post textarea"
+                  maxRows={10}
+                  maxLength={1000}
+                  //readOnly={readOnly}
+                  //onChange={handleTextChange}
+                  value={props.text}
+                  style={{
+                    resize: "none",
+                    width: "80%",
+                    border: "none",
+                    outline: "none",
+                    textAlign: "left",
+                  }}
+                />
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 };
