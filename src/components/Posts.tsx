d927931e493cda944105dbf20cf81fe00d1596d1
Amanda So <soamanda95@gmail.com>
2023-12-19 11:26:39 -0800

    limit posts shown to 5 initially and add show more function template

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -12,6 +12,17 @@ type post = {
 
 const Posts = (props): JSX.Element => {
   const [posts, setPosts] = useState<post[] | null>(null);
+  const [numOfPostsShown, setNumOfPostsShown] = useState(5);
+
+  const showMore = () => {
+    if (posts) {
+      if (numOfPostsShown + 5 <= posts.length) {
+        setNumOfPostsShown(numOfPostsShown + 5);
+      } else {
+        setNumOfPostsShown(posts.length);
+      }
+    }
+  };
   useEffect(() => {
     setPosts(props.posts);
   }, [props.posts]);
@@ -19,18 +30,19 @@ const Posts = (props): JSX.Element => {
   return (
     <>
       {posts
-        ? posts.map((post) => (
-            <Post
-              key={post._id}
-              authorid={post.author._id}
-              username={post.author.username}
-              //photo={profilePhotos[post.author._id]}
-              createdAt={post.createdAt}
-              updatedAt={post.updatedAt}
-              text={post.text}
-              postid={post._id}
-            />
-          ))
+        ? posts
+            .slice(0, numOfPostsShown)
+            .map((post) => (
+              <Post
+                key={post._id}
+                authorid={post.author._id}
+                username={post.author.username}
+                createdAt={post.createdAt}
+                updatedAt={post.updatedAt}
+                text={post.text}
+                postid={post._id}
+              />
+            ))
         : null}
     </>
   );
