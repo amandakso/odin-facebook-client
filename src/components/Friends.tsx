dae019fb4627a56c64eeca6b33f1d31d5845f91a
Amanda So <soamanda95@gmail.com>
2024-01-21 19:27:29 -0800

    fetch friends list of current user

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,4 +1,59 @@
+import { useEffect, useState } from "react";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+
+declare module "jwt-decode" {
+  export interface JwtPayload {
+    user: {
+      username: string;
+      _id: string;
+    };
+  }
+}
+
+type friend = {
+  createdAt: string; // date
+  recipient: string; // userid
+  requester: string; //userid
+  status: number; // 0-3
+  updatedAt: string; // date
+  __v: number;
+  _id: string; // friendship id
+};
+
 const Friends = () => {
+  const [profileFriends, setProfileFriends] = useState<Array<friend>>([]);
+
+  const token: string = sessionStorage.getItem("token") as string;
+  const decoded = jwtDecode<JwtPayload>(token);
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const res = await fetch(
+          `https://odin-facebook-api.onrender.com/api/users/${decoded.user._id}/friends`,
+          {
+            method: "GET",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const resJson = await res.json();
+        if (resJson.error) {
+          console.log(resJson.error);
+        } else {
+          setProfileFriends(resJson.friends);
+          console.log(resJson.friends);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (decoded.user._id) {
+      fetchFriends();
+    }
+  }, [decoded.user._id]);
   return (
     <>
       <h1>Friends Page</h1>
